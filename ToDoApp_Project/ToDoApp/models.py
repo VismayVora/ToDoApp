38068258319e3b068106c346c54b3a14baf3f8d3
Vismay Vora <voravismay9@gmail.com>
2021-08-20 11:23:28 +0530

    Created few models

--- ToDoApp_Project/ToDoApp/models.py
+++ ToDoApp_Project/ToDoApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+	title = models.CharField(max_length = 20)
+	description = models.TextField()
+	priority_no = models.IntegerField()
+	status = models.BooleanField()
+	#duedate = models.DateTimeField()
