630106dd155b3f5d0c971a964d87365ec0c816d3
Vismay Vora <voravismay9@gmail.com>
2021-08-21 20:57:14 +0530

    Increased max length of title

--- ToDoApp_Project/ToDoApp/models.py
+++ ToDoApp_Project/ToDoApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-	title = models.CharField(max_length = 20)
+	title = models.CharField(max_length = 50)
 	description = models.TextField()
 	priority_no = models.IntegerField()
 	status = models.BooleanField(default=False)
