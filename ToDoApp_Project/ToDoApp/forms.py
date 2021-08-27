dee30660fbcbe87d9d731ec7386746ce9156759c
Vismay Vora <voravismay9@gmail.com>
2021-08-27 22:31:34 +0530

    Minor Update

--- ToDoApp_Project/ToDoApp/forms.py
+++ ToDoApp_Project/ToDoApp/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Task
+from .models import Task,Task_Category
+
+class Task_CategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = Task_Category
+        fields = '__all__'
 
 class TaskForm(forms.ModelForm):
 
