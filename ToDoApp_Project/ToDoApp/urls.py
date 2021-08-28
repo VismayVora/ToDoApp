29c1816b76ea8bafda84167d897515f2e43dcaa4
Vismay Vora <voravismay9@gmail.com>
2021-08-28 11:10:12 +0530

    Applied ForeignKey for category of tasks, migrated and recreated superuser

--- ToDoApp_Project/ToDoApp/urls.py
+++ ToDoApp_Project/ToDoApp/urls.py
@@ -4,10 +4,14 @@ from ToDoApp_Project.settings import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, 
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('upload/', views.create_task, name = 'create-task'),
-    path('update/<int:task_id>', views.update_task),
-    path('delete/<int:task_id>', views.delete_task)
+    path('', views.categoryindex, name = 'categoryindex'),
+    path('task/', views.taskindex, name = 'taskindex'),
+    path('upload/', views.create_category, name = 'create-category'),
+    path('task/upload/', views.create_task, name = 'create-task'),
+    path('update/<int:task_category_id>', views.update_category),
+    path('task/update/<int:task_id>', views.update_task),
+    path('delete/<int:task_category_id>', views.delete_category),
+    path('task/delete/<int:task_id>', views.delete_task)
 ]
 
 if DEBUG:
--- ToDoApp_Project/ToDoApp/views.py
+++ ToDoApp_Project/ToDoApp/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, redirect
-from .forms import TaskForm
-from .models import Task
+from .forms import TaskForm,Task_CategoryForm
+from .models import Task,Task_Category
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def categoryindex(request):
+    task_category_list = Task_Category.objects.all()
+    return render(request, 'ToDoApp/task_category_list.html', {'task_category_list': task_category_list})
+
+def taskindex(request):
     task_list = Task.objects.all()
     return render(request, 'ToDoApp/task_list.html', {'task_list': task_list})
+
+def create_category(request):
+    upload = Task_CategoryForm()
+    if request.method == 'POST':
+        upload = Task_CategoryForm(request.POST, request.FILES)
+        if upload.is_valid():
+            upload.save()
+            return redirect('categoryindex')
+        else:
+            return HttpResponse("""your form is wrong, reload on <a href = "{{ url : 'categoryindex'}}">reload</a>""")
+    else:
+        return render(request, 'ToDoApp/category_upload_form.html', {'category_upload_form':upload})
 
 def create_task(request):
     upload = TaskForm()
@@ -14,29 +30,50 @@ def create_task(request):
         upload = TaskForm(request.POST, request.FILES)
         if upload.is_valid():
             upload.save()
-            return redirect('index')
+            return redirect('taskindex')
         else:
             return HttpResponse("""your form is wrong, reload on <a href = "{{ url : 'index'}}">reload</a>""")
     else:
         return render(request, 'ToDoApp/upload_form.html', {'upload_form':upload})
+
+def update_category(request, task_category_id):
+    task_category_id = int(task_category_id)
+    try:
+        task_sel = Task_Category.objects.get(id = task_category_id)
+    except Task_Category.DoesNotExist:
+        return redirect('categoryindex')
+    task_category_form = Task_CategoryForm(request.POST or None, instance = task_sel)
+    if task_category_form.is_valid():
+       task_category_form.save()
+       return redirect('categoryindex')
+    return render(request, 'ToDoApp/category_upload_form.html', {'category_upload_form':task_category_form})
 
 def update_task(request, task_id):
     task_id = int(task_id)
     try:
         task_sel = Task.objects.get(id = task_id)
     except Task.DoesNotExist:
-        return redirect('index')
+        return redirect('taskindex')
     task_form = TaskForm(request.POST or None, instance = task_sel)
     if task_form.is_valid():
        task_form.save()
-       return redirect('index')
+       return redirect('taskindex')
     return render(request, 'ToDoApp/upload_form.html', {'upload_form':task_form})
+
+def delete_category(request, task_category_id):
+    task_category_id = int(task_category_id)
+    try:
+        task_sel = Task_Category.objects.get(id = task_category_id)
+    except Task_Category.DoesNotExist:
+        return redirect('categoryindex')
+    task_sel.delete()
+    return redirect('categoryindex')
 
 def delete_task(request, task_id):
     task_id = int(task_id)
     try:
         task_sel = Task.objects.get(id = task_id)
     except Task.DoesNotExist:
-        return redirect('index')
+        return redirect('taskindex')
     task_sel.delete()
-    return redirect('index')
+    return redirect('taskindex')
