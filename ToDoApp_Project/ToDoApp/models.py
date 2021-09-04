8591146348ed8556793cd79f42be013e0187591c
Vismay Vora <voravismay9@gmail.com>
2021-09-04 22:07:27 +0530

    Completed signup along with image

--- ToDoApp_Project/ToDoApp/models.py
+++ ToDoApp_Project/ToDoApp/models.py
@@ -5,14 +5,16 @@ from django.contrib.auth.models import AbstractUser
 class AppUser(AbstractUser):
 	#EMAIL_FIELD = 'email'
 	#USERNAME_FIELD = 'username'
-	birth_date = models.DateField()
+	birth_date = models.DateField(null=True,blank=True)
 	profile_pic = models.ImageField(upload_to='images/')
 	#class Meta:
 		#db_table = 'auth_user'
 
 class Task_Category(models.Model):
+	categoryuser = models.ForeignKey(AppUser,on_delete = models.CASCADE, null=True,blank=True)
 	title = models.CharField(max_length = 50)
 	description = models.TextField(blank = True)
+	
 	def __str__(self):
 		return self.title
 
@@ -23,5 +25,9 @@ class Task(models.Model):
 	priority_no = models.PositiveIntegerField(blank = True)
 	status = models.BooleanField(default=False,blank = True)
 	duedate = models.DateTimeField(blank = True)
+	
 	def __str__(self):
 		return self.title
+
+	class Meta:
+		ordering = ['status']
