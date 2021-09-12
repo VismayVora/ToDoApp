f53e00c0d13404f37e12d1a0e95f498cb13eea96
Vismay Vora <voravismay9@gmail.com>
2021-09-12 19:23:43 +0530

    Revert "Personalization:User-specific categories are displayed,refined search fields,completed task 1" "Got back to the correct version"
    This reverts commit 5e3e60d66e80a301101129bd6597287bb87e90ba.

--- ToDoApp_Project/ToDoApp/forms.py
+++ ToDoApp_Project/ToDoApp/forms.py
@@ -26,13 +26,8 @@ class Task_CategoryForm(forms.ModelForm):
     class Meta:
         model = Task_Category
         fields = '__all__'
-        #Task_CategoryForm.fields["category"].queryset = Task_Category.objects.filter(user = request.user)
-        exclude = ['user'] #Excluded the user field and saved the current logged in user by creating an object instance[using form.save(commit = False)] in the view and overwriting the user with requet.user.
-"""
-If you don't exclude the user field from the form as done above, then you have to use this commented code to only give the option of choosing the current logged in user.
-    def __init__(self, request, *args, **kwargs):
-        super(Task_CategoryForm, self).__init__(*args, **kwargs)
-        self.fields['user'].queryset = AppUser.objects.filter(username=request.user)"""
+        Task_CategoryForm.user.queryset = Task_Category.objects.filter(user = request.user)
+        #exclude = ['user']
 
 
 class TaskForm(forms.ModelForm):
@@ -40,10 +35,5 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
-        exclude = ['user']
+        #exclude = ['user']
         #TaskForm.category.queryset = category.objects.filter()
-
-    def __init__(self, AppUser, *args, **kwargs):
-        super(TaskForm, self).__init__(*args, **kwargs)
-        self.fields['category'].queryset = Task_Category.objects.filter(user=AppUser)
-        #self.fields['user'].queryset = AppUser.objects.filter(username=request.user)"""
--- ToDoApp_Project/ToDoApp/views.py
+++ ToDoApp_Project/ToDoApp/views.py
@@ -30,13 +30,13 @@ class CategoryOwner(CreateView):
         return HttpResponseRedirect(self.get_success_url())"""
 
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def categoryindex(request):
     #task_category_list = Task_Category.objects.all()
     task_category_list = Task_Category.objects.filter(user = request.user)
     return render(request, 'ToDoApp/task_category_list.html', {'task_category_list': task_category_list})
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def taskindex(request):
     #task_list = Task.objects.all()
     task_list = Task.objects.filter(user = request.user)
@@ -47,68 +47,59 @@ def taskindex(request):
         #context['count'] = context['tasks'].filter(complete=False).count()
         #return context
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def create_category(request):
     upload = Task_CategoryForm()
     if request.method == 'POST':
         upload = Task_CategoryForm(request.POST, request.FILES)
         if upload.is_valid():
-           obj = upload.save(commit=False) # Return an object without saving to the DB
-           obj.user = request.user # Add an author field which will contain current user's id
-           obj.save() # Save the final "real form" to the DB
-           return redirect('ToDoApp:categoryindex')
+            upload.save()
+            return redirect('ToDoApp:categoryindex')
         else:
             return HttpResponse("""your form is wrong, reload on <a href = "{{ url : '/ToDoApp/categoryindex'}}">reload</a>""") #Have to check whether this works or not.
     else:
         return render(request, 'ToDoApp/category_upload_form.html', {'category_upload_form':upload})
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def create_task(request):
-    #upload = TaskForm()
+    upload = TaskForm()
     if request.method == 'POST':
-        upload = TaskForm(request.user, request.POST or None)
+        upload = TaskForm(request.POST, request.FILES)
         if upload.is_valid():
-            obj = upload.save(commit=False) # Return an object without saving to the DB
-            obj.user = request.user # The user field of the form will now contain current user's id. Could have also done: obj.user_id = request.user.id
-            obj.save() # Save the final "real form" to the DB
+            upload.save()
             return redirect('ToDoApp:taskindex')
         else:
             return HttpResponse("<a href='/ToDoApp/task'>Your form is wrong, click to reload.</a>")
     else:
-        upload = TaskForm(request.user)
         return render(request, 'ToDoApp/upload_form.html', {'upload_form':upload})
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def update_category(request, task_category_id):
     task_category_id = int(task_category_id)
     try:
         task_sel = Task_Category.objects.get(id = task_category_id)
     except Task_Category.DoesNotExist:
-        return redirect('ToDoApp: categoryindex')
+        return redirect('ToDOApp: categoryindex')
     task_category_form = Task_CategoryForm(request.POST or None, instance = task_sel)
     if task_category_form.is_valid():
-       obj = task_category_form.save(commit=False) # Return an object without saving to the DB
-       obj.user = request.user # Add an author field which will contain current user's id
-       obj.save() # Save the final "real form" to the DB
+       task_category_form.save()
        return redirect('ToDoApp:categoryindex')
     return render(request, 'ToDoApp/category_upload_form.html', {'category_upload_form':task_category_form})
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def update_task(request, task_id):
     task_id = int(task_id)
     try:
         task_sel = Task.objects.get(id = task_id)
     except Task.DoesNotExist:
         return redirect('ToDoApp:taskindex')
-    task_form = TaskForm(request.user, request.POST or None, instance = task_sel)
+    task_form = TaskForm(request.POST or None, instance = task_sel)
     if task_form.is_valid():
-       obj = task_form.save(commit=False) # Return an object without saving to the DB
-       obj.user = request.user # Add an author field which will contain current user's id
-       obj.save() # Save the final "real form" to the DB
+       task_form.save()
        return redirect('ToDoApp:taskindex')
     return render(request, 'ToDoApp/upload_form.html', {'upload_form':task_form})
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def delete_category(request, task_category_id):
     task_category_id = int(task_category_id)
     try:
@@ -118,7 +109,7 @@ def delete_category(request, task_category_id):
     task_sel.delete()
     return redirect('ToDoApp:categoryindex')
 
-@login_required(login_url = 'home')
+#@login_required(login_url = 'home')
 def delete_task(request, task_id):
     task_id = int(task_id)
     try:
